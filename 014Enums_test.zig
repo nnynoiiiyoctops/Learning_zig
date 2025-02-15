e7756e5330a09b9d13b54d4ef15d1a57c412224c
Nny_Noiiiy_Octopus <nny_noiiiy_octops@mail.ru>
2025-02-15 11:18:42 +0500

    Дополнил Enum

--- 014Enums_test.zig
+++ 014Enums_test.zig
@@ -8,18 +8,22 @@ test "Создание переменной пренадлежащей Enum" {
     try expect(@TypeOf(red_color) == Colors);
 }
 
-const TypedEnum = enum {
-    Top,
+const TEnum = enum {
+    var Top: u4 = 1;
     Left,
     Right,
     Down,
+    //По сути своей это все набор чисел от 0 до конца Enum
+    //Просто на этапе компиляции у них есть тип, и на этапе компиляции мы видем это как Enum. На деле это перечисление intов
 };
 
 test "Типизированный Enum" {
-    const tev: TypedEnum = TypedEnum.Top;
+    const tev: u4 = TEnum.Top;
     //tev - typed enum value
 
-    try expect(@TypeOf(tev) == TypedEnum);
+    try expect(tev == 1);
     //    try expect(@IntFromEnum(TypedEnum.Top) == 0); компилятор ругается на то что это недопустимая встроенная функция... Ну. Тогда это надо будет тестить в main
 
+    TEnum.Top += 1;
+    try expect(TEnum.Top == 2);
 }
