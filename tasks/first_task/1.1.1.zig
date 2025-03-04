da7d3cc18d2d1546dce00129123d747d56e052fb
Nny_Noiiiy_Octopus <nny_noiiiy_octops@mail.ru>
2025-03-04 21:27:36 +0500

    Распарсил числа

--- tasks/first_task/1.1.1.zig
+++ tasks/first_task/1.1.1.zig
@@ -1,13 +1,35 @@
 const std = @import("std");
 
 pub fn main() !void {
-
-    const input  = std.io.getStdOut().reader();
-    const output = std.io.getStdIn().writer();
-    var readed:[100:0]u8 = undefined;
+    const input = std.io.getStdIn().reader();
+    const output = std.io.getStdOut().writer();
+    var buffer: [100]u8 = undefined;
     
-    const len = try input.read(&readed);
-    readed[len] = 0;
+    var arg1: i64 = 0;
+    var arg2: i64 = 0;
+    var integers: [2]i64 = undefined;
 
-    _ = try output.write(readed[0..len+1]);
+
+    const readed = try input.read(&buffer);
+    var iterated = std.mem.splitAny(u8, buffer[0..readed-1], " ");
+
+    {
+        var index: usize = 0;
+        while (iterated.next()) |value| : (index += 1) {
+            if (value.len == 0) {
+                index -= 1;
+                continue;
+            }
+            if (index >= 2) break;
+//            const trimmed  = std.mem.trim(u8, value, " \r\n");
+            integers[index] = try std.fmt.parseInt(i64, value, 10);
+
+            //std.debug.print( "{} {s}", .{ @TypeOf(value), value});
+        }
+    }
+
+    arg1 = integers[0];
+    arg2 = integers[1];
+
+    _ = try output.write(buffer[0..readed-1]);
 }
